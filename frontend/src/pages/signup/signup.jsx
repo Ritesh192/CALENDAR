4fecc58d09c2db6fc4497639b8a91b70c746aee4
Ritesh192 <eie.20beic63@silicon.ac.in>
2023-06-24 10:20:23 +0530

    sign up and signin

---
+++ frontend/src/pages/signup/signup.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./signup.css";
+import {useNavigate} from "react-router-dom";
+import { Link } from "react-router-dom";
+
+function SignUp(){
+    const navigate =useNavigate();
+    const goSignIn = () => {
+        navigate('/signin');
+    }
+    return(
+        <div className="containerStyle">
+            <h1>SignUp</h1>
+            <input className="inputStyle" type="text" placeholder="Enter Name: "/>
+            <input className ="inputStyle" type="email" placeholder="Enter Email: "/>
+            <input className ="inputStyle" type="password" placeholder="Enter Password:" />
+            <input className ="inputStyle" type="password" placeholder="Confirm Password: "/>
+            <button className="buttonStyle" onClick={goSignIn}>SignUp</button>
+            Already Have An Account? <Link to="/signin">sign in</Link>     
+        </div>
+    );
+    
+}
+
+export default SignUp;
